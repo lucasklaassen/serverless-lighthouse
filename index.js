952466ac321e6feeaa86828b044fd8326292c179
lucasklaassen <lucasklaassen1@gmail.com>
2020-10-06 20:23:53 -0700

    add logging and chrome killing

--- index.js
+++ index.js
@@ -1,73 +1,83 @@
 const launcher = require("chrome-launcher");
-const chromium = require('chrome-aws-lambda');
-const lighthouse = require('lighthouse');
+const chromium = require("chrome-aws-lambda");
+const lighthouse = require("lighthouse");
+const log = require("lighthouse-logger");
 
-async function runLighthouse(url, chromeFlags = null, lighthouseFlags = null, lighthouseConfig = null) {
-    const flags = lighthouseFlags || {
-        disableCpuThrottling: true,
-        disableNetworkThrottling: true,
-        port: undefined
-    };
+async function runLighthouse(
+  url,
+  chromeFlags = null,
+  lighthouseFlags = null,
+  lighthouseConfig = null
+) {
+  const flags = lighthouseFlags || {
+    disableCpuThrottling: true,
+    disableNetworkThrottling: true,
+    port: undefined,
+  };
 
-    const defaultFlags = chromeFlags || [
-        '--headless',
-        '--disable-dev-shm-usage',
-        '--disable-gpu',
-        '--no-zygote',
-        '--no-sandbox',
-        '--single-process',
-        '--hide-scrollbars'
-    ];
+  const defaultFlags = chromeFlags || [
+    "--headless",
+    "--disable-dev-shm-usage",
+    "--disable-gpu",
+    "--no-zygote",
+    "--no-sandbox",
+    "--single-process",
+    "--hide-scrollbars",
+  ];
 
-    const lhConfig = lighthouseConfig || {
-        extends: 'lighthouse:default'
-    };
+  const lhConfig = lighthouseConfig || {
+    extends: "lighthouse:default",
+    logLevel: "info",
+  };
 
-    const launcherOptions = {
-        chromeFlags: defaultFlags
-    };
+  const launcherOptions = {
+    chromeFlags: defaultFlags,
+  };
 
-    //Only set the chromePath if actually executing in a lambda
-    if (process.env.LAMBDA_TASK_ROOT && process.env.AWS_EXECUTION_ENV) {
-        launcherOptions['chromePath'] = await chromium.executablePath;
+  log.setLevel('info');
+
+  //Only set the chromePath if actually executing in a lambda
+  if (process.env.LAMBDA_TASK_ROOT && process.env.AWS_EXECUTION_ENV) {
+    launcherOptions["chromePath"] = await chromium.executablePath;
+  }
+
+  // https://github.com/GoogleChrome/lighthouse/blob/master/docs/readme.md
+  let chrome = null;
+  try {
+    chrome = await launcher.launch(launcherOptions);
+    flags.port = chrome.port;
+    const results = lighthouse(url, flags, lhConfig);
+  } finally {
+    if (chrome) {
+      await chrome.kill();
     }
+  }
 
-    const command = await launcher.launch(launcherOptions)
-        .then((chrome) => {
-            flags.port = chrome.port;
-            return {
-                chrome,
-                start () {
-                    return lighthouse(url, flags, lhConfig)
-                }
-            }
-        });
-
-    return await command.start();
+  return await results;
 }
 
 function getDefaultChromeFlags() {
-    return [
-        '--headless',
-        '--disable-dev-shm-usage',
-        '--disable-gpu',
-        '--no-zygote',
-        '--no-sandbox',
-        '--single-process',
-        '--hide-scrollbars'
-    ];
+  return [
+    "--headless",
+    "--disable-dev-shm-usage",
+    "--disable-gpu",
+    "--no-zygote",
+    "--no-sandbox",
+    "--single-process",
+    "--hide-scrollbars",
+  ];
 }
 
 function getDefaultLighthouseFlags() {
-    return {
-        disableCpuThrottling: true,
-        disableNetworkThrottling: true,
-        port: undefined
-    };
+  return {
+    disableCpuThrottling: true,
+    disableNetworkThrottling: true,
+    port: undefined,
+  };
 }
 
 module.exports = {
-    runLighthouse: runLighthouse,
-    defaultChromeFlags: getDefaultChromeFlags(),
-    defaultLighthouseFlags: getDefaultLighthouseFlags()
+  runLighthouse: runLighthouse,
+  defaultChromeFlags: getDefaultChromeFlags(),
+  defaultLighthouseFlags: getDefaultLighthouseFlags(),
 };
